590758d89ba8850caec3ac61d9fdbb9afa84af85
Dougal Graham <dougalg@gmail.com>
2012-06-22 19:38:12 +0700

    Updated the script to only generate when you've modified your HTML

--- resume/print_pdf.php
+++ resume/print_pdf.php
@@ -1,25 +1,34 @@
 <?php
-include('wkhtmltopdf.php');
-$pdf = new WkHtmlToPdf(array(
-//    'no-outline',         // Make Chrome not complain
-// Why do we need to specify margins again?
-	'margin-top'    => '2cm',
-	'margin-right'  => '1.5cm',
-	'margin-bottom' => '2.5cm',
-	'margin-left'   => '2cm',
-));
 
-$pdf->setPageOptions(array(
- //   'disable-smart-shrinking',
-//    'user-style-sheet' => 'pdf.css',
-	'print-media-type',
-	'header-html' => 'blank.html',
-	'footer-html' => 'blank.html',
-	'header-spacing' => 2,
-	'footer-spacing' => 2
-));
+// What do you want to call your PDF?
+$PDFName = 'Dougal_Graham_-_CV.pdf';
+// If the PDF file is older than the HTML file then create it again, otherwise, just redirect
+if (filemtime($PDFName) > filemtime('index.html')) {
+	include('wkhtmltopdf.php');
+	$pdf = new WkHtmlToPdf(array(
+	//    'no-outline',         // Make Chrome not complain
+	// Why do we need to specify margins again?
+		'margin-top'    => '2cm',
+		'margin-right'  => '1.5cm',
+		'margin-bottom' => '2.5cm',
+		'margin-left'   => '2cm',
+	));
 
-$pdf->addPage('index.html');
+	$pdf->setPageOptions(array(
+	 //   'disable-smart-shrinking',
+	//    'user-style-sheet' => 'pdf.css',
+		'print-media-type',
+		'header-html' => 'blank.html',
+		'footer-html' => 'blank.html',
+		'header-spacing' => 2,
+		'footer-spacing' => 2
+	));
 
-$pdf->send();
+	$pdf->addPage('index.html');
+	$pdf->saveAs($PDFName);
+}
+
+// Redirect to the PDF
+header('Location: '.$PDFName);
+
 ?>
