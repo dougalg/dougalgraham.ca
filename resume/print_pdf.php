a0ba127058aab10e0611ce6d755296bf14f5e262
Dougal Graham <dougalg@gmail.com>
2012-06-22 12:07:40 +0700

    Margins!?

--- resume/print_pdf.php
+++ resume/print_pdf.php
@@ -3,16 +3,20 @@ include('wkhtmltopdf.php');
 $pdf = new WkHtmlToPdf(array(
 //    'no-outline',         // Make Chrome not complain
 // Why do we need to specify margins again?
-/*	'margin-top'    => '3cm',
+	'margin-top'    => '3cm',
 	'margin-right'  => '1.5cm',
 	'margin-bottom' => '2.5cm',
-	'margin-left'   => '2cm',*/
+	'margin-left'   => '2cm',
 ));
 
 $pdf->setPageOptions(array(
  //   'disable-smart-shrinking',
 //    'user-style-sheet' => 'pdf.css',
-	'print-media-type'
+	'print-media-type',
+	'header-html' => 'blank.html',
+	'footer-html' => 'blank.html',
+	'header-spacing' => 5,
+	'footer-spacing' => 5
 ));
 
 $pdf->addPage('index.html');
