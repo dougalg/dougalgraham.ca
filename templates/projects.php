935ed4a0a98b023152de1c556641691dad8f9118
Dougal Graham <dougalg@gmail.com>
2014-06-13 06:44:19 +0700

    Add keywords Styling

--- templates/projects.php
+++ templates/projects.php
@@ -13,6 +13,9 @@
 
                             <p>Downloadable paper based activities for teachers to use in class.</p>
 
+                            <ul class="keywords">
+                                <li>Python</li><li>CSS</li><li>HTML</li><li>Javascript</li><li>AJAX</li>
+                            </ul>
                         </div>
                     </div>
 
@@ -31,6 +34,9 @@
 
                             <p>I converted a PSD design to HTML/CSS</p>
 
+                            <ul class="keywords">
+                                <li>PHP</li><li>PSD Conversion</li><li>HTML</li><li>CSS</li>
+                            </ul>
                         </div>
                     </div>
 
@@ -43,6 +49,10 @@
                             <p>The World Association of Marching Show Bands hired me to upgrade their websites.</p>
 
                             <p>I upgraded an object-oriented PHP CSM, and modified the look and feel using HTML and CSS.</p>
+
+                            <ul class="keywords">
+                                <li>PHP</li><li>OOP</li><li>HTML</li><li>CSS</li><li>MySQL</li>
+                            </ul>
                         </div>
                     </div>
                 </article>
@@ -57,6 +67,10 @@
                             <p>A corpus keyword calculator.</p>
                             <p>A cross-platform calculator of Log Likelihood and Odds Ratio statistics.</p>
                             <p>Uses the British National Corpus as reference</p>
+
+                            <ul class="keywords">
+                                <li>Statistics</li><li>Corpus</li><li>Text Processing</li><li>Python</li>
+                            </ul>
                         </div>
                     </div>
                     <div class="portfolioItem clearfix" id="TypeThai">
@@ -67,6 +81,10 @@
                             <p>A Thai language input method for Mac OS X.</p>
                             <p>I used finite-state technology and objecive-c to develop an automatic guesser to redict which Thai word the user intended.</p>
                             <p>(Not an easy feat)</p>
+
+                            <ul class="keywords">
+                                <li>Objective-C</li><li>Thai</li><li>Finite-state processing</li><li>OSX</li>
+                            </ul>
                         </div>
                     </div>
                 </article>
