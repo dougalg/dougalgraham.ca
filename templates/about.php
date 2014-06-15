bc29c902b48858caa75d21da99a7af464575a28d
Dougal Graham <dougalg@gmail.com>
2014-06-15 08:23:12 +0700

    minor fixes

--- templates/about.php
+++ templates/about.php
@@ -1,6 +1,6 @@
             <img src="images/gravatar.jpg" class="my_photo">
 
-            <p>Hi, my name is Dougal Graham. I am a web developer, programmer, and linguist.</p>
+            <p>Hi, my name is Dougal Graham. I am a web developer, programmer, linguist, and teacher.</p>
 
             <p>I spend a lot of time building websites:</p>
             <dl>
@@ -18,7 +18,7 @@
                 <dd>Thai language input method</dd>
             </dl>
             
-            <p>I specialise in back end web programming, but also know my way around HTML and CSS. If you’d like to know a little <a href="#projects">more about my projects click here</a>.</p>
+            <p>I love both back and front-end coding. I love the challenge of finding the fastest, most elegent way to perform a task, but I also love the satisfaction of laying out my ideas visually in CSS and HTML. If you’d like to know a little <a href="/projects">more about my projects click here</a>.</p>
 
             <p>If you'd like to see what I've written, head on over to <a href="http://www.tfiaa.com/blog">my blog</a>, or take a look at <a href="http://th.linkedin.com/in/dougalg">my LinkedIn profile</a>.</p>
 
