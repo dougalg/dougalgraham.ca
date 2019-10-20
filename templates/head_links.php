c396e46e30d2124f0bf5e752724726bd69961085
Dougal Graham <dougalg@gmail.com>
2019-10-20 22:06:20 +0700

    Out with the old

--- templates/head_links.php
+++
@@ -1,7 +0,0 @@
-<?php
-    foreach ($pages as $page => $name) {
-        $selected = ($page == $current_page) ? 'selected' : '';
-        echo "
-                <a href=\"/$page\" class=\"$selected\">$name</a>";
-    }
-?>
