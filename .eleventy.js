0d99a2e547627422c37cefad204804159f39b112
Dougal Graham <dougalg@gmail.com>
2019-10-21 09:31:23 +0700

    Minify html/css, add favicon, fix broken lists

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,20 @@
+const htmlmin = require('html-minifier');
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/images");
+	eleventyConfig.addPassthroughCopy("src/images");
+
+	eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+		if(outputPath.endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+				minifyCSS: true,
+				minifyJS: true,
+			});
+			return minified;
+		}
+
+		return content;
+	});
 };
