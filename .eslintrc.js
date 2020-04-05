a0ff95f25524e727b2122691dc528dcaa5ba64f9
dougalg <dougalg@gmail.com>
2020-04-05 10:00:40 +0700

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-  }
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended'
+	],
+	// add your custom rules here
+	rules: {
+		"indent": ["error", "tab"],
+		"no-tabs": ["error", { allowIndentationTabs: true }],
+		"vue/html-indent": ["error", "tab"]
+	}
 }
